3064234da27b3017586aa66e1b7a1a3fa94d31d7
Bruno Luz <bsilva.se@gmail.com>
2023-12-21 16:08:53 -0300

    main - Adicionando depências: Jackson

---
+++ src/main/java/dev/bstk/okkutil/mapper/modelmapper/Mapper.java
@@ -0,0 +1,24 @@
+package dev.bstk.okkutil.mapper.modelmapper;
+
+import org.modelmapper.ModelMapper;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Mapper {
+
+  private static final ModelMapper MODEL_MAPPER = new ModelMapper();
+
+  private Mapper() { }
+
+  public static <T> T to(final Object source, final Class<T> clazz) {
+    return MODEL_MAPPER.map(source, clazz);
+  }
+
+  public static <S, T> List<T> list(final List<S> source, final Class<T> clazz) {
+    return source
+      .stream()
+      .map(element -> MODEL_MAPPER.map(element, clazz))
+      .collect(Collectors.toList());
+  }
+}
