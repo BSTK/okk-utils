c2fd83dbec31a840e8c0449db5469e653bfe481d
Bruno Luz <bsilva.se@gmail.com>
2023-12-21 16:12:01 -0300

    main - Adicionando Json utils

---
+++ src/main/java/dev/bstk/okkutil/json/jackson/Json.java
@@ -0,0 +1,38 @@
+package dev.bstk.okkutil.json.jackson;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+
+public class Json {
+
+  private static final String ERROR_MESSAGE = "Unable to parse!";
+  private static final ObjectMapper JSON_MAPPER = new ObjectMapper();
+
+  private Json() { }
+
+  public static <T> T toObject(final byte[] payload, final Class<T> clazz) {
+    try {
+      return JSON_MAPPER.readValue(payload, clazz);
+    } catch (IOException ex) {
+      throw new IllegalArgumentException(ERROR_MESSAGE, ex);
+    }
+  }
+
+  public static <T> T toObject(final Object object, final Class<T> clazz) {
+    try {
+      final var payload = toString(object);
+      return JSON_MAPPER.readValue(payload, clazz);
+    } catch (IOException ex) {
+      throw new IllegalArgumentException(ERROR_MESSAGE, ex);
+    }
+  }
+
+  public static String toString(final Object object) {
+    try {
+      return JSON_MAPPER.writeValueAsString(object);
+    } catch (IOException ex) {
+      throw new IllegalArgumentException(ERROR_MESSAGE, ex);
+    }
+  }
+}
